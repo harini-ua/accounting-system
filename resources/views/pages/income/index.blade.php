a9f333812253c55df9aa84d2b9fe02847c5eb3da
Sergey Harini <harini.ua@gmail.com>
2020-07-23 15:53:28 +0300

    change filters markup

--- resources/views/pages/income/index.blade.php
+++ resources/views/pages/income/index.blade.php
@@ -27,17 +27,22 @@
     <!-- Sidebar Area Ends -->
     <!-- list -->
     <section class="list-wrapper section">
-        <div class="card">
-            <div class="card-content">
-                <!-- Options and filter dropdown button-->
-                <x-date-filter start="{{ $startDate }}" end="{{ $endDate }}" table="incomes-table"/>
-                <x-filter
-                        table="incomes-table"
-                        :options="$clients"
-                        url="{{ route('incomes.index') }}"
-                        name="client_filter"
-                        title="Filter by Client"
-                />
+        <div class="card-panel accounts-page-card-panel">
+            <div class="row">
+                <div class="col s12 m12 l6 input-field">
+                    <x-date-filter start="{{ $startDate }}" end="{{ $endDate }}" table="incomes-table"/>
+
+                </div>
+                <div class="col s12 l6 display-flex align-items-center show-btn">
+                    <x-filter
+                            table="incomes-table"
+                            :options="$clients"
+                            url="{{ route('incomes.index') }}"
+                            name="client_filter"
+                            title="Filter by Client"
+                    />
+                    <x-reset-filters/>
+                </div>
             </div>
         </div>
         <div class="page-layout">
@@ -100,7 +105,7 @@
                 </div>
             </div>
         </div>
-           </section>
+    </section>
 @endsection
 
 {{-- vendor scripts --}}
