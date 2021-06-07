540977683a8e1bdbca2061d219b2f8603362d7db
Sergey Harini <harini.ua@gmail.com>
2021-06-07 19:27:38 +0300

    test dd

--- app/Http/Controllers/ContractController.php
+++ app/Http/Controllers/ContractController.php
@@ -29,8 +29,10 @@ class ContractController extends Controller
         ];
 
         $pageConfigs = ['pageHeader' => true, 'isFabButton' => true];
-
+        dd($pageConfigs);
 //        $clients = Client::all()->sortBy('name');
+
+
         //$salesManagers = Person::byPosition(Position::SalesManager)->orderBy('name')->get();
 //        $status = ContractStatus::toCollection();
 
