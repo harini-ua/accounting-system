889c71de5b404bfba7a8991b6c2454507aca4c16
Sergey Harini <harini.ua@gmail.com>
2021-06-15 18:39:57 +0300

    fix start_date filter

--- resources/views/components/date-filter.blade.php
+++ resources/views/components/date-filter.blade.php
@@ -1,10 +1,10 @@
 <div class="date-filter" data-table="{{ $table }}" data-url="{{ $url }}">
     <div class="invoice-filter-action input-field date-filter-block">
-        <input type="text" id="from" name="start_date_filter" class="custom-datepicker-from" value="{{ $start ?? '' }}" readonly>
+        <input type="text" id="from" name="start_date" class="custom-datepicker-from" value="{{ $start ?? '' }}" readonly>
         <span class="custom-filter-label {{ isset($start) ? __('active') : __('') }}">From</span>
     </div>
     <div class="invoice-filter-action input-field date-filter-block">
-        <input type="text" id="to" name="end_date_filter" class="custom-datepicker-to" value="{{ $end ?? '' }}" readonly>
+        <input type="text" id="to" name="end_date" class="custom-datepicker-to" value="{{ $end ?? '' }}" readonly>
         <span class="custom-filter-label {{ isset($end) ? __('active') : __('') }}">To</span>
     </div>
 </div>
